1003ccabfeafc2e673f35a2fa963ca027ed95258
Osman Ungur <osmanungur@gmail.com>
2018-02-08 15:05:21 +0100

    Refactoring, support for batch request and html output

--- src/main/java/nl/youwe/diff/controller/DiffController.java
+++ src/main/java/nl/youwe/diff/controller/DiffController.java
@@ -10,33 +10,53 @@ package nl.youwe.diff.controller;
  */
 
 import name.fraser.neil.plaintext.diff_match_patch;
+import nl.youwe.diff.dto.BatchDiffRequest;
+import nl.youwe.diff.dto.BatchDiffResponse;
 import nl.youwe.diff.dto.DiffRequest;
+import nl.youwe.diff.service.DiffService;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class DiffController {
 
+    private final DiffService diffService;
+
+    private final Logger logger;
+
+    @Autowired
+    public DiffController(DiffService diffService, Logger logger) {
+        this.diffService = diffService;
+        this.logger = logger;
+    }
+
+
     @PostMapping("/diff")
     public LinkedList<diff_match_patch.Diff> diffAction(@RequestBody @Valid DiffRequest diffRequest) {
-        diff_match_patch diffMatchPatch = new diff_match_patch();
-        diffMatchPatch.Diff_Timeout = diffRequest.getTimeout();
+        return diffService.getDiff(diffRequest.getContext(), diffRequest.getOptions());
+    }
 
-        LinkedList<diff_match_patch.Diff> diffLinkedList = diffMatchPatch.diff_main(diffRequest.getOriginal(), diffRequest.getRevised(), diffRequest.isCheckLines());
+    @PostMapping("/diff/html")
+    public String htmlDiffAction(@RequestBody @Valid DiffRequest diffRequest) {
+        return diffService.toPrettyHtml(
+                diffService.getDiff(diffRequest.getContext(), diffRequest.getOptions())
+        );
+    }
 
-        if (diffRequest.getCleanupSemantic()) {
-            diffMatchPatch.diff_cleanupSemantic(diffLinkedList);
-        }
-
-        if (diffRequest.getCleanupSemanticLossless()) {
-            diffMatchPatch.diff_cleanupSemanticLossless(diffLinkedList);
-        }
-
-        return diffLinkedList;
+    @PostMapping("/diff/batch")
+    public List<BatchDiffResponse> batchDiffAction(@RequestBody @Valid BatchDiffRequest batchDiffRequest) {
+        return batchDiffRequest.getContext()
+                .stream()
+                .map(batchContext -> new BatchDiffResponse(batchContext.getIdentifier(), diffService.getDiff(batchContext, batchDiffRequest.getOptions())))
+                .collect(Collectors.toList());
     }
 
 }
